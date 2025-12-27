16f26f068e11d60ac5b6b40b78d91605a786c2f7
Mayankjha12 <mayank.jha.ug24@nsut.ac.in>
2025-12-27 14:55:38 +0530

    Update Feedback.js

--- Backend/models/Feedback.js
+++ Backend/models/Feedback.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+
 const FeedbackSchema = new mongoose.Schema({
     rating: Number,
     writtenFeedback: String,
-    voiceTranscript: String, // Voice feedback ke liye
+    voiceTranscript: String, 
     createdAt: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model("Feedback", FeedbackSchema);
