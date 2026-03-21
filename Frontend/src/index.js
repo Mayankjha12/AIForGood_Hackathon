5565eb5d01c372650184f0556148368d0e7e850e
Mayankjha12 <mayankkumarjha523@gmail.com>
2026-03-21 10:03:17 +0530

    Fixed missing LanguageContext for Netlify

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'; 
-// Yahan 'App' ka 'A' capital hona chahiye agar aapne file rename kar di hai
 import App from './App'; 
+import { LanguageProvider } from './contexts/LanguageContext'; // Ye line add karo
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <LanguageProvider>
+      <App />
+    </LanguageProvider>
   </React.StrictMode>
 );
